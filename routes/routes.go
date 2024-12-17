5bd80f29f7759ae3c0c7db8d7acec854da06c8fc
RichieMuga <richiemugambi4@gmail.com>
2024-12-17 22:50:30 +0300

    feat: addd repository initialization and signup route

--- routes/routes.go
+++ routes/routes.go
@@ -2,14 +2,25 @@
 package routes
 
 import (
-  "github.com/RichieMuga/go-gin-template/controllers"
+	"github.com/RichieMuga/go-gin-template/controllers"
+	"github.com/RichieMuga/go-gin-template/internal/repositories"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-// InitializeRoutes defines the initialaization of routes with api versioning
-func InitializeRoutes(router *gin.Engine) {
+// InitializeRoutes defines the initialization of routes with api versioning
+func InitializeRoutes(router *gin.Engine, db *gorm.DB) {
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/ping", controllers.Ping)
+		
+		// Initialize the UserRepository using the DB instance
+		userRepo := repositories.NewUserRepository(db)
+		
+		// Initialize the UserController with the userRepo instance
+		userController := controllers.NewUserController(userRepo)
+		
+		// User routes
+		v1.POST("/signup", userController.CreateUser)
 	}
 }
