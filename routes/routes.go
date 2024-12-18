d7c500e4f781f368b225496ef4227da1c7d2e501
RichieMuga <richiemugambi4@gmail.com>
2024-12-18 20:55:36 +0300

    feat: add protected test route and protected route setup

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ package routes
 import (
 	"github.com/RichieMuga/go-gin-template/controllers"
 	"github.com/RichieMuga/go-gin-template/internal/repositories"
+	"github.com/RichieMuga/go-gin-template/routes/middlewares"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -12,7 +13,18 @@ import (
 func InitializeRoutes(router *gin.Engine, db *gorm.DB) {
 	v1 := router.Group("/api/v1")
 	{
+    // add cors for jwt
+    router.Use(middlewares.ConfigureCORS())
+
+    // add protected routes setup
+    authenticated := v1.Group("/")
+    authenticated.Use(middlewares.Authenticate)
+
+    // Initialize testing route
 		v1.GET("/ping", controllers.Ping)
+
+    // Initialize protected route testing
+    authenticated.GET("/protectedRoute", controllers.ProtectedRoute)
 		
 		// Initialize the UserRepository using the DB instance
 		userRepo := repositories.NewUserRepository(db)
