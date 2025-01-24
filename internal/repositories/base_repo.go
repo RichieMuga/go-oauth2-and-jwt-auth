165cd433eb40ed0a6bf65b401facbcd43b659642
RichieMuga <richiemugambi4@gmail.com>
2025-01-24 18:03:23 +0300

    chore: add comments to explain what the base repo does

--- internal/repositories/base_repo.go
+++ internal/repositories/base_repo.go
@@ -1,16 +1,19 @@
-// base_repo.go contains the boilerplate for creating new repo to avoid alot of duplication
+// Package repositories contains the boilerplate for creating new repo to avoid alot of duplication
 package repositories
 
 import "gorm.io/gorm"
 
+// BaseRepo is a config base for new repositories to be created
 type BaseRepo struct {
 	db *gorm.DB
 }
 
+// NewBaseRepo instanciates the NewBaseRepo function
 func NewBaseRepo(db *gorm.DB) *BaseRepo {
 	return &BaseRepo{db: db}
 }
 
+// DB contains the db to be used
 func (b *BaseRepo) DB() *gorm.DB {
 	return b.db
 }
